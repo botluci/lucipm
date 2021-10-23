8ac974cd058c350445491b92e5aa1c56deed4d7f
Luci <68446614+botluci@users.noreply.github.com>
2021-10-23 22:52:55 +0400

    Zaaa

--- plugins/help.py
+++ plugins/help.py
@@ -16,12 +16,7 @@ async def help_me(bot, message):
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     await bot.send_message(chat_id=message.chat.id, 
-        text=Presets.WELCOME_TEXT.format(info.first_name),
-        reply_markup=InlineKeyboardMarkup(
-[
-InlineKeyboardButton(f"👉🏻 Luciw", url=" https://t.me/luciw")
-]
-)
+        text=Presets.WELCOME_TEXT.format(info.first_name)
 
     )
     await bot.send_message(
