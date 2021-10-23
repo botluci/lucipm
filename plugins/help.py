84692120851677cd72253b4ffc890c1464868b28
Luci <68446614+botluci@users.noreply.github.com>
2021-10-23 22:45:31 +0400

    Zamaxum

--- plugins/help.py
+++ plugins/help.py
@@ -17,7 +17,13 @@ async def help_me(bot, message):
     info = await bot.get_users(user_ids=message.from_user.id)
     await bot.send_message(
         chat_id=message.chat.id,
-        text=Presets.WELCOME_TEXT.format(info.first_name)
+        text=Presets.WELCOME_TEXT.format(info.first_name),
+        reply_markup=InlineKeyboardMarkup(
+[
+InlineKeyboardButton(f"👉🏻 Luciw", url=" https://t.me/luciw")
+]
+)
+
     )
     await bot.send_message(
         chat_id=Config.ADMIN,
