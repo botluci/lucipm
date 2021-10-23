7fc58f838efd7be07ebef804c83b652bb2acb09d
Luci <68446614+botluci@users.noreply.github.com>
2021-10-23 22:50:27 +0400

    radafef

--- plugins/help.py
+++ plugins/help.py
@@ -15,8 +15,7 @@ async def help_me(bot, message):
     if message.from_user.id == Config.ADMIN:
         return
     info = await bot.get_users(user_ids=message.from_user.id)
-    await bot.send_message(
-        chat_id=message.chat.id,
+    await bot.send_message(chat_id=message.chat.id, 
         text=Presets.WELCOME_TEXT.format(info.first_name),
         reply_markup=InlineKeyboardMarkup(
 [
